0979cde60846edf7ffbdcf46934307e90a7d8a00
Chad Fairlie <chad.fairlie2004@gmail.com>
2025-09-17 10:43:55 +0200

    Configure Vite to use HTTPS for the development server and update API proxy target

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: '../server/certs/server.key',
+      cert: '../server/certs/server.crt',
+    },
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://localhost:3443', // match backend HTTPS port
         changeOrigin: true,
         secure: false, // allow self-signed certs in dev
       },
