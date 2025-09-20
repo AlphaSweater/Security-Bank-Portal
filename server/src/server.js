b2372a9613dbd121645f15920b36d9974625433c
AlphaSweater <104318840+AlphaSweater@users.noreply.github.com>
2025-09-20 17:19:16 +0200

    Added Tasks

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,19 @@
-
 import fs from "fs";
 import https from "https";
 import http from "http";
 import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import app from "./app.js";
 import logger from "./logger.js";
 
 dotenv.config();
 
-const certPath = path.resolve(process.env.SSL_PATH || "./certs");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const certPath = path.resolve(
+  process.env.SSL_PATH || path.join(__dirname, "../certs")
+);
 const options = {
   key: fs.readFileSync(path.join(certPath, "server.key")),
   cert: fs.readFileSync(path.join(certPath, "server.crt")),
@@ -18,15 +22,15 @@ const options = {
 const HTTPS_PORT = process.env.HTTPS_PORT || 5000;
 const HTTP_PORT = process.env.HTTP_PORT || 8080;
 
-
 https.createServer(options, app).listen(HTTPS_PORT, () => {
   logger.info(`✅ HTTPS server running on port ${HTTPS_PORT}`);
 });
 
-
-http.createServer((req, res) => {
+http
+  .createServer((req, res) => {
     res.writeHead(301, { Location: "https://" + req.headers.host + req.url });
     res.end();
-  }).listen(HTTP_PORT, () => {
+  })
+  .listen(HTTP_PORT, () => {
     logger.info(`🌍 HTTP redirect server running on port ${HTTP_PORT}`);
   });
