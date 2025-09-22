8184ae564459587421e7b04e38ecceaee923a034
AlphaSweater <104318840+AlphaSweater@users.noreply.github.com>
2025-09-22 16:27:21 +0200

    Refactor User model to implement database integration for user operations

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,7 +1,16 @@
-// Placeholder for DB integration later
-export default class User {
-  constructor(email, passwordHash) {
-    this.email = email;
-    this.passwordHash = passwordHash;
-  }
+import { getDB } from "../config/db.js";
+import { ObjectId } from "mongodb";
+
+const collection = () => getDB().collection("users");
+
+export async function insertUser(doc) {
+  return await collection().insertOne(doc);
+}
+
+export async function findUserByEmail(email) {
+  return await collection().findOne({ email });
+}
+
+export async function findUserById(id) {
+  return await collection().findOne({ _id: new ObjectId(id) });
 }
