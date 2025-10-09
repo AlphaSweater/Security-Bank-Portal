54c4822aa7a6270199bc0e80010a43ffd97d878e
AlphaSweater <104318840+AlphaSweater@users.noreply.github.com>
2025-10-09 15:29:24 +0200

    Add rate limiting to authentication and user routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,6 +3,11 @@ import * as authController from "#controllers/authController.js";
 import { validateData } from "#middlewares/validationMiddleware.js";
 import * as userValidation from "#utils/validation/userValidation.js";
 import { asyncHandler } from "#utils/asyncHandler.js";
+import { addRateLimiter } from "#middlewares/rateLimitMiddleware.js";
+import {
+  AuthRateLimiter,
+  AuthLoginRateLimiter,
+} from "#config/rateLimitConfig.js";
 
 const router = express.Router();
 
@@ -12,14 +17,20 @@ router.get("/sessionCheck", asyncHandler(authController.sessionCheck));
 // POST endpoints for authentication
 router.post(
   "/login",
+  ...addRateLimiter(AuthLoginRateLimiter),
   validateData(userValidation.loginUserSchema),
   asyncHandler(authController.login)
 );
 router.post(
   "/register",
+  ...addRateLimiter(AuthRateLimiter),
   validateData(userValidation.registerUserSchema),
   asyncHandler(authController.register)
 );
-router.post("/logout", asyncHandler(authController.logout));
+router.post(
+  "/logout",
+  ...addRateLimiter(AuthRateLimiter),
+  asyncHandler(authController.logout)
+);
 
 export default router;
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,17 +3,30 @@ import * as userController from "#controllers/userController.js";
 import requireAuth from "#middlewares/authMiddleware.js";
 import { requireRole } from "#middlewares/roleMiddleware.js";
 import { asyncHandler } from "#utils/asyncHandler.js";
+import { addRateLimiter } from "#middlewares/rateLimitMiddleware.js";
+import { GeneralRateLimiter } from "#config/rateLimitConfig.js";
 
 const router = express.Router();
 
-router.get("/me", requireAuth, userController.getMe);
+router.get(
+  "/me",
+  ...addRateLimiter(GeneralRateLimiter),
+  requireAuth,
+  userController.getMe
+);
 
 // Protected dashboard route
-router.get("/dashboard", requireAuth, userController.getDashboard);
+router.get(
+  "/dashboard",
+  ...addRateLimiter(GeneralRateLimiter),
+  requireAuth,
+  userController.getDashboard
+);
 
 // Example: only admin can access
 router.get(
   "/admin-data",
+  ...addRateLimiter(GeneralRateLimiter),
   requireAuth,
   requireRole("admin"),
   userController.getAdminData
